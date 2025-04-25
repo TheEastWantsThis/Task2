661332ffd5c366e102db99a53440c32360e56e2d
TheEastWantsThis <reworldtoday@gmail.com>
2025-04-25 17:07:06 +0300

    С учетом пятого задания

--- migrations/20250425083814_tasks.up.sql
+++ migrations/20250425083814_tasks.up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE tasks (
                        id SERIAL PRIMARY KEY,
                        task VARCHAR(255) NOT NULL,
-                       is_done BOOLEAN DEFAULT FALSE,
-                       deleted_at TIMESTAMP DEFAULT NULL
+                       is_done BOOLEAN DEFAULT FALSE
 );
